c354f11e324bf0995beaca468596f96f8e132207
dodan <dodanik13@gmail.com>
2024-08-27 19:40:32 +0300

    0.1

--- keyboards/user/create_kb_promoactions.py
+++ keyboards/user/create_kb_promoactions.py
@@ -7,7 +7,7 @@ async def create_kb_promoactions(lang):
     kb_settings_user.button(text="RU_Promotions", callback_data="name_baner_ru")
     kb_settings_user.button(text="EN_promotions", callback_data="name_baner_ru")
 
-    # Настраиваем количество кнопок в рядке
+    # Настраиваем количество кнопок в рядке.
     kb_settings_user.adjust(2)
 
     return kb_settings_user.as_markup(resize_keyboard=True)
